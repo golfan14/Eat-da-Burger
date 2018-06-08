f6ae7c47d76ca5d7b6c816ba4adf164a7964533e
Stephen Ermisch <stephenermisch@Stephens-MacBook-Pro.local>
2018-06-07 18:18:50 -0600

    connections

---
+++ config/orm.js
@@ -0,0 +1,43 @@
+var connection = require("../config/connection.js");
+
+var orm = {
+
+    selectAll: function (tableInput, cb) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    insertOne: function (table, cols, vals, cb) {
+        var queryString = "INSERT INTO " + table;
+
+        console.log(queryString);
+
+        connection.query(queryString, vals, function (err, result) {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    },
+    updateOne: function (table, objColVals, condition, cb) {
+        var queryString = "UPDATE " + table;
+
+        console.log(queryString);
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    }
+};
+
+module.exports = orm;
+
+
